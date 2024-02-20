eef518e6b1faf6d7a8477abc7fbaf2a271885e44
mayyu9 <coolmahimsd@gmail.com>
2024-02-20 12:44:48 +0530

    Adding blocking calls to understand the architecture

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const fs = require('fs');
+const crypto = require('crypto');
+
+const start = Date.now(); // start time of server
+
+process.env.UV_THREADPOOL_SIZE = 5; // this is by which we can control the thread size.
 
 setTimeout(() => {
     console.log('set timeout 0')
@@ -14,6 +19,26 @@ fs.readFile('sample.txt', 'utf-8', () => {
     setTimeout(() => {
         console.log('inside the io operation 2')
     }, 10 * 100);
+
+    // adding blocking calls by adding crypto
+    crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () =>{
+        console.log(`${Date.now() -start}ms password1 hashing....`)
+    });
+
+    crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () =>{
+        console.log(`${Date.now() -start}ms password2 hashing....`)
+    })
+
+    crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () =>{
+        console.log(`${Date.now() -start}ms password3 hashing....`)
+    })
+    crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () =>{
+        console.log(`${Date.now() -start}ms password4 hashing....`)
+    })
+    crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () =>{
+        console.log(`${Date.now() -start}ms password5 hashing....`)
+    })
+
 });
 
 console.log('Hello from nodejs server');
@@ -29,3 +54,8 @@ console.log('Hello from nodejs server');
 // inside the io operation 1 -> as this callbacks register later, event loop gets executed once more to finish this two callbacks
                                 //as this was expired callback, so gets executed first.
 // inside the io operation 2 -> after 10sec this timer gets expired so this gets executed.
+
+// when we add some blocking code, when we have 4 blocking blocking calls and we have 4 threads in default. execution time for all
+// the process is almost same as all four blocking calls execute on a separate thread so time is almost same.
+
+// when we add 5th blocking call, time taken by 5th is more because the thread was busy doing other task.
